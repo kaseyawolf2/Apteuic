8bb9ab7bd99e3d303edf56f7b3e058e03236529a
Kasey Keahey'git config --global user.name Kasey <Kasey_keahey@yahoo.com>
2018-04-15 16:09:50 -0500

    added linux building

--- Scripts/Build.sh
+++ Scripts/Build.sh
@@ -6,15 +6,14 @@ g++ -c -o Main      ./../../main.cpp
 g++ -c -o Cell      ./../../Cell.cpp
 g++ -c -o Console   ./../../Console.cpp
 g++ -c -o File      ./../../File.cpp
-g++ -c -o Window    ./../../Window.cpp
+g++ -c -o Window     ./../../Window.cpp
 
 echo "Objects Built"
 
-libs= -lSDL2main -lSDL2
 
 cd ./../
 
-g++ -g -o Build ./Objects/* $libs
+g++ -g -o Build ./Objects/* -lSDL2main -lSDL2
 
 echo
 './Build'
